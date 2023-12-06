2bef1a1a0b9140e1a834ac40c5249960ba624cb7
Konstantin Pinzhenin <dir.pka@gmail.com>
2023-12-06 20:45:09 +0500

    Create separate func for changing phase to both side
    Create func for determine next phase to switch for both side rotation
    func separate from the func for table's aligned value

--- main.c
+++ main.c
@@ -10,6 +10,15 @@
 #define INIT_ANGLE_VALUE 17000
 #define ROTATION_SIGN_TO_SHIFT(RS) ( (RS) - 2 )
 
+//global var
+struct phase_t sequence[6] = {
+    {.phase_name = ST1G, .i2c_pair = 1},
+    {.phase_name = ST2G, .i2c_pair = 1},
+    {.phase_name = ST1Y, .i2c_pair = 1},
+    {.phase_name = ST2Y, .i2c_pair = 1},
+    {.phase_name = ST1R, .i2c_pair = 1},
+    {.phase_name = ST2R, .i2c_pair = 1}   
+};
 
 // use git to look changes !!!!!
 int main(int argc, char **argv)
@@ -19,8 +28,8 @@ int main(int argc, char **argv)
     // init tbl
     Tmp_aligned_tbl_Init(&aligned_tbl[0], sizeof(aligned_tbl) / 8 );
     
-    union aligned_pos *ptr_tbl = &aligned_tbl[4];
-    
+    //union aligned_pos *ptr_tbl = &aligned_tbl[4];
+    /*
     for(int i = 0; i < 10; i++)
     {
         // mean clockwise rotation
@@ -38,7 +47,23 @@ int main(int argc, char **argv)
         
         ptr_tbl = ptr_next;
     }
+    */
     
+    int arr[3] = {0, 1, 2};
+    void *p_arr = &arr[0];
+    printf("{0} =%d, +1 = %d \n", *((int*)p_arr), *((int*)p_arr + 1) );
+    struct phase_t *ptr_phase = &sequence[1];
+    struct phase_t *ptr_cw_phase = NextSwitchedPhase(ROTATION_SIGN_TO_SHIFT(ROTATION_CLOCKWISE) * 1,
+                                    ptr_phase, sequence,
+                                    sizeof(sequence) / sizeof(sequence[0]) );
+    printf("next CW phase = %d \n", ptr_cw_phase->phase_name);
+    for (int i = 0; i < 13; i++)
+    {
+        printf("phase_name {%d} \n", ptr_phase->phase_name);
+        ptr_phase = NextSwitchedPhase(ROTATION_SIGN_TO_SHIFT(ROTATION_CLOCKWISE) * 2,
+                                    ptr_phase, sequence,
+                                    sizeof(sequence) / sizeof(sequence[0]) );
+    }
 	printf("hello world\n");
 	return 0;
 }
@@ -59,6 +84,24 @@ unsigned* NextShiftedElement(int shift, unsigned *ptr_a, unsigned a[], int size)
                 ptr_a =&a[71];
     }
     return ptr_a;
+}
+
+struct phase_t* NextSwitchedPhase(int shift, struct phase_t *p_ph, struct phase_t arr_ph[], int size)
+{
+    // return pointer to next phase swithes
+        if (shift > 0)
+    {
+        for(int i = 0; i < shift; i++)
+            if (p_ph++ == &arr_ph[size - 1])
+                p_ph = &arr_ph[0];
+    }
+    else
+    {
+        for (int i = 0; i > shift; i--)
+            if(p_ph-- == &arr_ph[0])
+                p_ph =&arr_ph[size-1];
+    }
+    return p_ph;
 }
 
 void Tmp_aligned_tbl_Init(union aligned_pos *tbl, unsigned tbl_size)
--- ttype.h
+++ ttype.h
@@ -1,5 +1,14 @@
 #ifndef TTYPE_H_
 #define TTYPE_H_
+
+// phase name
+#define ST1G 0
+#define ST2G 1
+#define ST1Y 2
+#define ST2Y 3
+#define ST1R 4
+#define ST2R 5
+
 typedef struct
 {
 	unsigned val :16; 				   // aligned position val
@@ -14,8 +23,15 @@ union aligned_pos
 	unsigned all;
 };
 
+struct phase_t
+{
+    const unsigned phase_name;
+    unsigned i2c_pair;
+};
+
 void Tmp_aligned_tbl_Init(union aligned_pos *tbl, unsigned tbl_size);
 
 unsigned * NextShiftedElement(int shift, unsigned *ptr_a, unsigned a[], int size);
+struct phase_t* NextSwitchedPhase(int shift, struct phase_t *p_ph, struct phase_t arr_ph[], int size);
 
 #endif
